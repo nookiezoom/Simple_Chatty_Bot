2d5ce3ffc4492077d78aa64f1e34a3ab6af758fe
Sergey Svynaryov <nookiezoom@gmail.com>
2020-07-20 19:42:22 +0300

    Task 16 resolved

---
+++ Problems/Good rest on vacation/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Vacation vacation = new Vacation(scanner.nextInt(), scanner.nextInt(),
+                scanner.nextInt(), scanner.nextInt());
+        System.out.println(vacation.calculatePrice());
+    }
+
+    private static class Vacation {
+        private int durationInDays;
+        private int totalFoodCostPerDay;
+        private int oneWayFlightCost;
+        private int costOfOneNightInHotel;
+
+        Vacation(int durationInDays, int totalFoodCostPerDay,
+                 int oneWayFlightCost, int costOfOneNightInHotel) {
+            this.durationInDays = durationInDays;
+            this.totalFoodCostPerDay = totalFoodCostPerDay;
+            this.oneWayFlightCost = oneWayFlightCost;
+            this.costOfOneNightInHotel = costOfOneNightInHotel;
+        }
+
+        public int getDurationInDays() {
+            return durationInDays;
+        }
+
+        public void setDurationInDays(int durationInDays) {
+            this.durationInDays = durationInDays;
+        }
+
+        public int getTotalFoodCostPerDay() {
+            return totalFoodCostPerDay;
+        }
+
+        public void setTotalFoodCostPerDay(int totalFoodCostPerDay) {
+            this.totalFoodCostPerDay = totalFoodCostPerDay;
+        }
+
+        public int getOneWayFlightCost() {
+            return oneWayFlightCost;
+        }
+
+        public void setOneWayFlightCost(int oneWayFlightCost) {
+            this.oneWayFlightCost = oneWayFlightCost;
+        }
+
+        public int getCostOfOneNightInHotel() {
+            return costOfOneNightInHotel;
+        }
+
+        public void setCostOfOneNightInHotel(int costOfOneNightInHotel) {
+            this.costOfOneNightInHotel = costOfOneNightInHotel;
+        }
+
+        public int calculatePrice() {
+            return 2 * oneWayFlightCost
+                    + durationInDays * totalFoodCostPerDay
+                    + (durationInDays - 1) * costOfOneNightInHotel;
+        }
+    }
+}
